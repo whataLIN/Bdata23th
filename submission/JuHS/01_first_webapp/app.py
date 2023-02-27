57b13918cff9d9ff1b890a992942f11e8acb33b5
Ju Hyunsung <126255274+Hyunsung1994@users.noreply.github.com>
2023-02-27 22:30:57 +0900

    이미지 삽입

--- submission/JuHS/01_first_webapp/app.py
+++ submission/JuHS/01_first_webapp/app.py
@@ -6,6 +6,8 @@ st.write(
     """
     # 나에게 가장 도움이 될 것 같은 유튜브
     ## 빵형의 개발도상국
+    [링크](http://naver.com)
+![이미지에 대한 설명](https://ouch-cdn2.icons8.com/I1jo3kiWiqFTrx9iGCXvMvgOraN1a1ToBW03QbC7mbw/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMzAx/L2I5ZjYyY2ZkLTU2/YzQtNDc5Zi1hMzBk/LWQxYjljZjJjNjUz/NS5wbmc.png)
     !(https://w7.pngwing.com/pngs/914/758/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png)
     > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
