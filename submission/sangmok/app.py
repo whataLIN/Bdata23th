1927191c326274abd4b12b443f53ea9f98c7664b
Sangmok-Kim <126433941+Sangmok-Kim@users.noreply.github.com>
2023-02-27 17:49:02 +0900

    숙제 내준 상태

--- submission/sangmok/app.py
+++ submission/sangmok/app.py
@@ -11,13 +11,22 @@ st.write(
 
     # 숙제 : 이 밑을 마크다운으로 채워오기
 
-    # 유튜브 추천 (for me / for classmate)
+    # 유튜브 추천 (1. for me 2. for classmate)
+    1. 내용
+    이유
+    2. 내용
+    이유
+    ########
 
-    #
+    # 참고 사이트 - 
+    * [icons8](https://icons8.com/)
+    * [flaticon]
+    * [pixabay]
 
     """
 
-
+    # Streamlit에서 deploy할 시 이용해야 할 url: submission/sangmok/app.py
+    # pull request로 제출
 
 )
 
