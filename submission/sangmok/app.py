9ba402cd16c62484d08de6e5cc74cc024312c2ab
Sangmok-Kim <126433941+Sangmok-Kim@users.noreply.github.com>
2023-02-27 17:42:09 +0900

    ㅇㅇㅇㅇ

---
+++ submission/sangmok/app.py
@@ -0,0 +1,41 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 제 첫 웹페이지 입니다
+    ## 부족하지만 많이 사랑해주세요!
+    * 1$ = 1,300원
+    * ^_^
+
+    # 숙제 : 이 밑을 마크다운으로 채워오기
+
+    # 유튜브 추천 (for me / for classmate)
+
+    #
+
+    """
+
+
+
+)
+
+
+
+# https://pixabay.com/ko
+st.image(
+    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
+
+
+
+
+
+)
+
+
+
+
+### 숙제
+
+
