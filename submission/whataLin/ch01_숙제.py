46ff62f520bdfb787364dd83aa407cc9b5dc475e
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-02-27 19:38:53 +0900

    선 삽입

--- submission/whataLin/ch01_숙제.py
+++ submission/whataLin/ch01_숙제.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.write("""## 어떤 유튜브 채널을 알아볼까?""")
+st.write("""## 어떤 유튜브 채널을 알아볼까?
+            ---
+            \n""")
 whichChannel=[None,"나에게 가장 도움이 될 것 같은 유튜브", "다른 학생들에게 가장 도움이 될 것 같은 유튜브"]
 my_choice = st.selectbox('선택하세요', whichChannel)
 
