d57f93d998f886c2bfdbc46be166118424415a9c
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-02-27 20:28:06 +0900

    d

--- submission/cc5547/01_first_webapp/app.py
+++ submission/cc5547/01_first_webapp/app.py
@@ -12,6 +12,8 @@ st.write(
     
 
     # 1. 나에게 가장 도움이 될 것 같은 유튜브
+    [표시될 텍스트](https://naver.com)<br>
+    ![이미지에 대한 설명](https://ouch-cdn2.icons8.com/llLHKo43gAdT2ZjeLQl1u4V_Gt5c8h59In2dgFsJXg0/rs:fit:256:256/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNjY0/LzRiZTZiMzkyLTlk/NWUtNDA2Zi04ZTAx/LWRlODYzMjQ2YzBi/My5wbmc.png)
     > ### `빵형의 개발 도상국`
     ##### 이유
     1. 그냥
@@ -19,12 +21,12 @@ st.write(
     1. 이냥
 
 
-    ## 빵형의 개발 도상국
-    > -> 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
-
     # 2. 다른 학생들에게 가장 도움이 될 것 같은 유튜브 
-    ## 노마드코덤
-    > -> 이유 : 최신 IT트렌드를 쉽고 재미 있게 잘 전해준다.
+     > ### `빵형의 개발 도상국`
+    ##### 이유
+    1. 그냥
+    1. 저냥
+    1. 이냥
 
 
     # 참고 사이트
