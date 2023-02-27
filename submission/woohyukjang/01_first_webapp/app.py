1afab500b5ad87221cc851c45dd62ac438cb95e3
woohyuk jang <51698571+woohyukjang@users.noreply.github.com>
2023-02-27 20:25:02 +0900

    ㅇㅇ

--- submission/woohyukjang/01_first_webapp/app.py
+++ submission/woohyukjang/01_first_webapp/app.py
@@ -14,59 +14,23 @@ st.write(
     |[마크다운 링크](https://the-fat-cat.notion.site/Markdown-42c07b9f12be439c9d6a9f1a240ade4b)|
     |[Github](https://github.com/woohyukjang)|
  
-    ##강사님 추천 강의
-
-    > 내가 들어야 할 강의
-
-    >>[나도코딩](https://www.youtube.com/watch?v=kWiCuklohdY&t=10s) 
     
-    ###듣다 손놓은 6시간 강의 완강하자
+    #강사님 추천 강의
+
+    내가 들어야 할 강의
+
+    >[나도코딩](https://www.youtube.com/watch?v=kWiCuklohdY&t=10s) 
+    
+    듣다 손놓은 6시간 강의 완강하자
 
     
-    > 학원 동기들에게 추천하는 강의
+    학원 동기들에게 추천하는 강의
 
-    >>[혁펜하임](https://www.youtube.com/@hyukppen)
+    >[혁펜하임](https://www.youtube.com/@hyukppen)
 
-    ###머신러닝 이론을 정말 잘 설명해 주심
+    머신러닝 이론을 정말 잘 설명해 주심
     
-    # 정리
-    # 제목 (Headline)
-    ## 2단계 제목
-    ### 3단계 제목
-    #### 4단계
-    ##### 5단계
-    수평선
-    ---
-    텍스트에 ---를 바로 붙이면 `#`과 같은 효과
-
-    (수평선)
-
-    ---
-    **굵게**
-    *기울게*
-    __굵게__
-    _기울게_
-    ~~취소선~~
-    <u>밑줄</u>
-    <mark>형광펜</mark>
     
-    > 인용문
-    >> 인용문 안에 인용문
-    >>> 인용문 안에 인용문 안에 인용문
-    >>>> 인용문 안에 인용문 안에 인용문 안에 인용문
-
-    >>이유:
-
-    ## 부족하지만 많이 사랑해주세요!
-
-    # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
-
-    ##
-
-    >>이유:
-
-    * 1$ = 1,300원
-    * ^_^dqjkwfhgkeljarfhalkejrfhalkef
 
 
     # 참고 사이트
