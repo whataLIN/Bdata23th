c8715b76840d736368173d2c6664594280d30f76
heecong <125568892+heecong@users.noreply.github.com>
2023-02-27 18:21:08 +0900

    링크 추가

--- submission/Heecong/01_first_webapp/app.py
+++ submission/Heecong/01_first_webapp/app.py
@@ -5,14 +5,15 @@ import streamlit as st
 st.write(
     """
     ### 나에게 가장 도움이 될 것 같은 유튜브
-    #### <span style="color:#2EFEF7">**생활코딩**</span>
+    #### <mark>**생활코딩**</mark>
     > 이유 : 썸네일이 깔끔해서 보고싶은 동영상을 잘 찾아볼 수 있을 것 같다.
+    > [링크](https://www.youtube.com/@coohde)
 
     ### 다른 학생들에게 가장 도움이 될 것 같은 유튜브
-    ####<span style="color:#2EFE9A">**노마드코더**</span>
+    ####<mark>**노마드코더**</mark>
     > 이유 : 유명한 채널이고 코딩말고도 개발자들을 위한 팁들도 있어서 유용할 것 같다.
+    > [링크](https://www.youtube.com/@nomadcoders)
 
-    <span style="color:#d3d3d3">#d3d3d3</span>
 
 
     # 참고 사이트
