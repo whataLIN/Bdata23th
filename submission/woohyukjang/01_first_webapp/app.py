b77a320b6331b5502f02e56896c22d13959b1be4
woohyuk jang <51698571+woohyukjang@users.noreply.github.com>
2023-02-27 18:47:52 +0900

    경로변경3

--- submission/woohyukjang/01_first_webapp/app.py
+++ submission/woohyukjang/01_first_webapp/app.py
@@ -20,6 +20,40 @@ st.write(
 
     * 1$ = 1,300원
     * ^_^
+
+
+    # 참고 사이트
+    * [icons8](https://icons8.com)
+    * [flation](https://flaticon.com)
+    * [pixabay](https://pixabay.com/ko/)
+
+
+    """
+)
+
+# https://pixabay.com/ko
+st.image(
+    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
+)
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 나에게 가장 도움이 될 것 같은 유튜브
+
+    ##
+
+    >>이유:
+
+    ## 부족하지만 많이 사랑해주세요!
+
+    # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
+
+    ##
+
+    >>이유:
+
+    * 1$ = 1,300원
+    * ^_^
     """
 )
 
