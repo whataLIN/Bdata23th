8446b7d3e8ce1bc0e7729407f7a60f2234f0d374
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-02-27 19:17:59 +0900

    선택버튼

--- submission/whataLin/ch01_숙제.py
+++ submission/whataLin/ch01_숙제.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-DATE_COLUMN = 'date/time'
+st.write("""##어떤 유튜브 채널을 알아볼까?""")
 
+my_choice = st.selectbox('선택', whichChannel)
 
-st.write(
+if my_choice==1:
+    st.write(
+
     """
 
     ## 나에게 가장 도움이 될 것 같은 유튜브
@@ -13,9 +16,14 @@ st.write(
 
     > 이유: 새로운 머신러닝/딥러닝 지식들이 많다
 
+    """
+    )
 
 
+if my_choice==2:
+    st.write(
 
+    """
     ## 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     #### 노마드코더
     ![유튜브 이미지](https://yt3.googleusercontent.com/ytc/AL5GRJWCKbcOn_YX6jhiTrNoYBV3pg0eWu41jb2zpkXHjA=s88-c-k-c0x00ffffff-no-rj)
@@ -23,4 +31,4 @@ st.write(
 
     > 이유 : 최신 IT 트렌드를 쉽고 재밌게 전달해준다.
     """
-)
+    )
