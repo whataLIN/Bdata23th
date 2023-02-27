34746025e5541ddfed2f1e14a782ebad9ee9740f
Ju Hyunsung <126255274+Hyunsung1994@users.noreply.github.com>
2023-02-27 22:46:52 +0900

    얄코 추가

--- submission/JuHS/01_first_webapp/app.py
+++ submission/JuHS/01_first_webapp/app.py
@@ -9,12 +9,12 @@ st.write(
     
     # 나에게 가장 도움이 될 것 같은 유튜브
     
-    ![이미지에 대한 설명](https://yt3.ggpht.com/ytc/AL5GRJVzjSwh0Mv6I6bjCN2vlPvtPnZOohnr4U0WF1xL8XI=s68-c-k-c0x00ffffff-no-rj)
-    ## 얄팍한 코딩사전
-    
     ![이미지에 대한 설명](https://i.ytimg.com/vi/1I3hMwQU6GU/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLADnpGfxOX13n9Sp78ug6cFst-XPw)
-    ### 빵형의 개발도상국
-    
+    ## [얄팍한 코딩사전](https://www.youtube.com/@yalco-coding)
+
+    > 구독자 : 8.58만명
+
+
     > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
     ## 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     ### 노마드코더
