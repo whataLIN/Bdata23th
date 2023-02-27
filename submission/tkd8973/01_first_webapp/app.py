2e8a50e4efc3a992265554c4082256e7aaf7ead9
tkd8973 <126433959+tkd8973@users.noreply.github.com>
2023-02-27 18:03:50 +0900

    youtube_url

--- submission/tkd8973/01_first_webapp/app.py
+++ submission/tkd8973/01_first_webapp/app.py
@@ -22,17 +22,24 @@ st.write(
     > ctrl + m + a : 현재 셀 기준 위로 코드셀 생성
 
     > ctrl + m + b : 현재 셀 기준 아래에 코드셀 생성
-    
+
     > ctrl + m + t : 현재 셀 기준 아래에 텍스트셀 생성
 
     ---
     ## 나에게 가장 도움이 될 것 같은 유튜브
     ### 혁펜하임
     > 머신러닝, 딥러닝에 대한 기초 수학과 tensorflow, pytorch 등 머신러닝 플랫폼 사용법을 알 수 있고, 더 깊은 지식을 쌓을 수 있기 때문이다
+
+    > * [혁펜하임](https://www.youtube.com/@hyukppen)
+
     ## 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     ### 조코딩
     > 입문자를 위한 기초 코딩 뿐만 아니라 빠른 개발자 트렌드 변화에 맞춰 OpenAI와 최신 트렌드 내용들을 커뮤니티에 업로드 해주기 때문에 코딩에 익숙하지 않은 사람들도 편안하게 볼 수 있다.
+    
+    > * [조코딩](https://www.youtube.com/@jocoding)
 
+
+    ---
     ## 참고 사이트
     * [icons8](https://icons8.com/)
     * [flaticon](https://www.flaticon.com/)
