b5f7e41d316407f1ccc91f1160703f9d9a9fed1b
Ju Hyunsung <126255274+Hyunsung1994@users.noreply.github.com>
2023-02-27 23:09:03 +0900

    구분선

--- submission/JuHS/01_first_webapp/app.py
+++ submission/JuHS/01_first_webapp/app.py
@@ -21,11 +21,15 @@ st.write(
     #### 추천영상 : [제대로 파는 Git & GitHub - 깃 끝.장.내.기](https://www.youtube.com/watch?v=1I3hMwQU6GU)
     ![이미지에 대한 설명](https://i.ytimg.com/vi/1I3hMwQU6GU/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLADnpGfxOX13n9Sp78ug6cFst-XPw)
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    ---
+
+    -----
+
+    - - - - -
+
+    * * *
+
+    *******
 
     ## 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     ### 노마드코더
