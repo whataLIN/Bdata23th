b5ba93fb2c12e76524d96c1f5eb20196ac667829
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-02-27 20:03:43 +0900

    d

--- submission/cc5547/01_first_webapp/app.py
+++ submission/cc5547/01_first_webapp/app.py
@@ -24,9 +24,23 @@ st.write(
 
 
     # 참고 사이트
+    * # 개발전반
     * [생활코딩](https://www.youtube.com/@coohde/)
     * [노마드 코](https://www.youtube.com/@nomadcoders/)
     * [조코딩](https://www.youtube.com/@jocoding/)
+    * [코딩하는거니](https://www.youtube.com/@gunnycoding/)
+    * [코딩애플](https://www.youtube.com/@codingapple/)
+    * [나도코딩](https://www.youtube.com/@nadocoding/)
+    * [드림코딩](https://www.youtube.com/@dream-coding/)
+    * [독학하는 프로그래머 김플](https://www.youtube.com/@kimfl/)
+
+    * #Data/AL/ML/DL
+    * [오늘의코드](https://www.youtube.com/@todaycode/)
+    * [퇴근후딴짓](https://www.youtube.com/@ai-study/)
+    * [빵형의 개발도상국](https://www.youtube.com/@bbanghyong/)
+    * [혁펜하임](https://www.youtube.com/@hyukppen/)
+    * [메타코드M](https://www.youtube.com/@mcodeM/)
+    * [AISchool](https://www.youtube.com/@aischool_ai/)
 
      
     * 1$ = 1,300원
