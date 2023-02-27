fe849fab2cf35532e646df09ee289ab164a7ad3e
JaejinLee1215 <70628245+JaejinLee1215@users.noreply.github.com>
2023-02-27 23:23:05 +0900

    과제1

--- submission/JaejinLee1215/01_first_webapp/app.py
+++ submission/JaejinLee1215/01_first_webapp/app.py
@@ -5,20 +5,24 @@ import streamlit as st
 st.write(
     """
     # 나에게 가장 도움이 될 것 같은 유튜브
-    ## 빵형의 개발도상국
-    > 이유 : 블라블라
+
+    ---
+    ## [메타코드M](https://www.youtube.com/@mcodeM)
+    > 이유 : 기술 면접이나 새로운 트렌드 기술을 잘 알려주고 논문 등 개념에 대한 리딩법의 가독성이 좋다.
 
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
-    ## 노마드코더
-    > 이유 : 최신 it 트렌드를 쉽고 재밌게 전달해준다
 
-    * 1$ = 1,300원
-    * ^_^
-    
+    ---
+    ## [드림코딩](https://www.youtube.com/@dream-coding)
+    > 이유 : 개발에 아예 처음이시거나 개발자의 현실 등을 잘 보여주고 어렵지 않게 트렌드를 설명해준다.
+
+    ```
     # 참고 사이트
     * [icons8](https://icons8.com/)
     * [flaticon](https://www.flaticon.com/)
     * [pixabay](https://pixabay.com/ko/)
+    ```
+
     """
 )
 
