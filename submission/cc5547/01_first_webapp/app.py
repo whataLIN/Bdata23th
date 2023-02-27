462c9bb1fb5842c0097f90ddf0b11a84d11184fa
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-02-27 18:12:36 +0900

    저장

---
+++ submission/cc5547/01_first_webapp/app.py
@@ -0,0 +1,37 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    # """ """ : 여러 줄을 묶어서 표시할 수 있는 문자열
+    # ''' '''(동일 기능)
+    
+    """
+    # 제 첫 웹페이지 입니다
+    ## 부족하지만 많이 사랑해주세요!
+    * 1$ = 1,300원
+    * ^_^
+
+
+
+    #나에게 가장 도움이 될 것 같은 유튜브
+    ## 빵형의 개발 도상국
+    > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
+
+    # 다른 학생들에게 가장 도움이 될 것 같은 유튜브 
+    ## 노마드코덤
+    > 이유 : 최신 IT트렌드를 쉽고 재미 있게 잘 전해준다.
+
+
+    # 참고 사이트
+    *[icons8](https://icons8.com/)
+    *[flaticon](https://flaticon.com/)
+    *[pixabay](https://pixabay.com/)
+    """
+
+)
+
+# https://pixabay.com/ko
+st.image(
+    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
+)
