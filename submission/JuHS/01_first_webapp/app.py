23a74d8806a9d7626c3f54a3c4364e3d06ce099e
Ju Hyunsung <126255274+Hyunsung1994@users.noreply.github.com>
2023-02-27 22:34:16 +0900

    이미지 삽입

--- submission/JuHS/01_first_webapp/app.py
+++ submission/JuHS/01_first_webapp/app.py
@@ -6,7 +6,7 @@ st.write(
     """
     # 나에게 가장 도움이 될 것 같은 유튜브
     ## 빵형의 개발도상국
-    ![이미지에 대한 설명](https://w7.pngwing.com/pngs/914/758/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png)
+    ![이미지에 대한 설명](https://drive.google.com/file/d/1rV8bxQGVfa9S2OZAHD1Gqo-cnosXQaX5/view?usp=sharing)
     > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     ## 노마드코더
