27dfc4efb4f927b9c42b6e22815f51066e088e44
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-02-27 19:05:52 +0900

    링크 포함

--- submission/whataLin/ch01_숙제.py
+++ submission/whataLin/ch01_숙제.py
@@ -5,16 +5,17 @@ DATE_COLUMN = 'date/time'
 
 st.write(
     """
-    <details>
+
     ## 나에게 가장 도움이 될 것 같은 유튜브
-    <summary>
     ### 빵형의 개발도상국
+    [link](https://www.youtube.com/@bbanghyong)
     > 이유: 새로운 머신러닝/딥러닝 지식들이 많다
-    </summary>
-    </details>
+
+
 
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
-    ## 노마드코더
+    ### 노마드코더
+    [link](https://www.youtube.com/@nomadcoders)
     > 이유 : 최신 IT 트렌드를 쉽고 재밌게 전달해준다.
     """
 )
