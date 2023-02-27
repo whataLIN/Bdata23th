bf5c53172327a294a134995140260153fd0dc6ef
woohyuk jang <51698571+woohyukjang@users.noreply.github.com>
2023-02-27 19:51:56 +0900

    ㅇㅇ

--- submission/woohyukjang/01_first_webapp/app.py
+++ submission/woohyukjang/01_first_webapp/app.py
@@ -6,12 +6,12 @@ st.write(
     """
     # 장우혁 공부방  *Since 2023-02-27*
     ---
-    
-    ##링크모음
 
-    >[수업 자료 링크](https://the-fat-cat.notion.site/26-ef748e37e6fa4dfc821ce8972dd9c5fe)
-    >[slack 링크](https://app.slack.com/client/T04NR00MM5F/C04NC7H3K0B/rimeto_profile/U04QX4NB2PR)
-    >[마크다운 링크](https://the-fat-cat.notion.site/Markdown-42c07b9f12be439c9d6a9f1a240ade4b)
+    >링크모음
+
+    >>[수업 자료 링크](https://the-fat-cat.notion.site/26-ef748e37e6fa4dfc821ce8972dd9c5fe)
+    >>[slack 링크](https://app.slack.com/client/T04NR00MM5F/C04NC7H3K0B/rimeto_profile/U04QX4NB2PR)
+    >>[마크다운 링크](https://the-fat-cat.notion.site/Markdown-42c07b9f12be439c9d6a9f1a240ade4b)
     
     # 정리
     # 제목 (Headline)
