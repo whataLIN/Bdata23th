5c7c01a834be56aa8215be70006acf1be7b12860
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-02-27 19:10:02 +0900

    이미지 추가

--- submission/whataLin/ch01_숙제.py
+++ submission/whataLin/ch01_숙제.py
@@ -8,6 +8,7 @@ st.write(
 
     ## 나에게 가장 도움이 될 것 같은 유튜브
     #### 빵형의 개발도상국
+    ![유튜브 이미지](https://yt3.googleusercontent.com/ytc/AL5GRJWEjUOIX_yyyHbeg73QKMQarL5Nu2qBrk3BXwyE=s88-c-k-c0x00ffffff-no-rj)
     [link](https://www.youtube.com/@bbanghyong)
 
     > 이유: 새로운 머신러닝/딥러닝 지식들이 많다
@@ -17,6 +18,7 @@ st.write(
 
     ## 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     #### 노마드코더
+    ![유튜브 이미지](https://yt3.googleusercontent.com/ytc/AL5GRJWCKbcOn_YX6jhiTrNoYBV3pg0eWu41jb2zpkXHjA=s88-c-k-c0x00ffffff-no-rj)
     [link](https://www.youtube.com/@nomadcoders)
 
     > 이유 : 최신 IT 트렌드를 쉽고 재밌게 전달해준다.
