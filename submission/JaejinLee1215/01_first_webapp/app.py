cf52918780edfb77cfc2b3d7be614f49df22a507
JaejinLee1215 <70628245+JaejinLee1215@users.noreply.github.com>
2023-02-27 23:54:58 +0900

    과제1

--- submission/JaejinLee1215/01_first_webapp/app.py
+++ submission/JaejinLee1215/01_first_webapp/app.py
@@ -6,7 +6,7 @@ st.write(
     """
 
     
-    # 나에게 가장 도움이 될 것 같은 유튜브
+    # 나에게 가장 도움이 될 것 같은 유튜브 [](https://cdn-icons-png.flaticon.com/512/6172/6172533.png)
     
     ---
     ## [메타코드M](https://www.youtube.com/@mcodeM) [![](https://yt3.googleusercontent.com/5tbNEN4mSD0NC1vWGWPX1Y4YSEPVEUhuXTM9eSW4Prb_m-TUW1f9NBpAVLVLHWwx2-ob9DpnCA=s88-c-k-c0x00ffffff-no-rj)](https://www.youtube.com/@mcodeM)
@@ -18,7 +18,7 @@ st.write(
     
     ---
     ## [드림코딩](https://www.youtube.com/@dream-coding) [![](https://yt3.googleusercontent.com/ytc/AL5GRJUv6U4FMuV5lDt_eZEHefddVBHcE1jEbm7roAMo4Q=s88-c-k-c0x00ffffff-no-rj)](https://www.youtube.com/@dream-coding)
-    > **<mark>이유</mark>** 
+    > **이유** 
     >>개발에 아예 처음이시거나 개발자의 현실 등을 잘 보여주고 어렵지 않게 트렌드를 설명해준다.
 
     
