154898a94c99b8d8f2d29a333fdcfd5ee981430b
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-02-28 03:13:45 +0900

    update16

--- submission/01_first_webapp/app.py
+++ submission/01_first_webapp/app.py
@@ -12,7 +12,7 @@ st.balloons()
 
 st.write(
     """
-    # 코딩 유튜브 채널 추천❗  
+    ## 코딩 유튜브 채널 추천❗  
     """
 )
 
@@ -27,7 +27,7 @@ with col1 :
   # column 1 에 담을 내용
   st.write(
     """
-    #### 나에게 가장 도움이 될 것 같은 유튜브
+    ##### 나에게 가장 도움이 될 것 같은 유튜브
     """)
 
   st.image("https://user-images.githubusercontent.com/71927533/221631776-7815c4a2-6500-4c1b-ba7a-208b451972ee.jpg")
@@ -37,33 +37,8 @@ with col2 :
   # column 2 에 담을 내용
   st.write(
     """
-    #### 남이 보면 좋을 것 같은 유튜브
+    ##### 남이 보면 좋을 것 같은 유튜브
     """)
   st.image("https://user-images.githubusercontent.com/71927533/221631810-b72fa62f-2c41-4a86-a105-2f4a0c1e1b2c.jpg")
   st.info('추천 이유 : IT 트렌드 흐름을 알기 쉽고 빠르게 설명해주고, 간단 명료합니다!', icon="ℹ️")
 
-
-# with 구문 말고 다르게 사용 가능 
-col1.subheader(' i am column1  subheader !! ')
-col2.checkbox('this is checkbox2 in col2 ') 
-#=>위에 with col2: 안의 내용과 같은 기능을합니다
-
-
-
-
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
-
-col = st.columns(1)
-
-with col:
-    text_input = st.text_input(
-        "추천하고 싶은 채널을 입력해 주세요! 👇",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
-
-    if text_input:
-        st.write("회원님의 추천 채널은 ", text_input, "입니다!")
