d4721a06277f67a464ac04681f09ef9e600f2e39
Ju Hyunsung <126255274+Hyunsung1994@users.noreply.github.com>
2023-02-27 22:48:41 +0900

    줄 바꾸기

--- submission/JuHS/01_first_webapp/app.py
+++ submission/JuHS/01_first_webapp/app.py
@@ -11,9 +11,8 @@ st.write(
     
     ![이미지에 대한 설명](https://i.ytimg.com/vi/1I3hMwQU6GU/hq720.jpg?sqp=-oaymwEjCOgCEMoBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLADnpGfxOX13n9Sp78ug6cFst-XPw)
     ## [얄팍한 코딩사전](https://www.youtube.com/@yalco-coding)
-
     > 구독자 : 8.58만명
-    
+
     > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
 
 
