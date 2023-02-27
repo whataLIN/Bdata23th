0c6d13da13d36d5a01bd0eccebca058f51e5c8ee
Kimdongha96 <126434022+Kimdongha96@users.noreply.github.com>
2023-02-27 18:04:03 +0900

    streamlit

--- material/01_first_webapp/app.py
+++ material/01_first_webapp/app.py
@@ -4,14 +4,15 @@ import streamlit as st
 # 마크다운을 기반으로 한 꾸미기 기능 작동
 st.write(
     """
-    # 제 첫 웹페이지 입니다
-    ## 부족하지만 많이 사랑해주세요!
-    * 1$ = 1,300원
-    * ^_^
-    """
-)
-
-# https://pixabay.com/ko
-st.image(
-    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
-)
+    #나에게 가장 도움이 될 것 같은 유튜브
+  ## 빵형의 개발도상국
+  > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
+  
+  # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
+  ## 노마드코더
+  > 이유 : 최신 IT 트렌드를 쉽고 재밌게 전달해준다
+  #참고사이트
+  [icons8](https://icons8.com)
+  [flaticon](https://flaticon.com)
+  [pixabay](https://pixabay.com)
+   
