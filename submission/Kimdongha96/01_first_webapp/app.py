c78bf122eae31713df662f5069e9f2691b8ef7bd
Kimdongha96 <126434022+Kimdongha96@users.noreply.github.com>
2023-02-27 18:42:03 +0900

    streamlit

--- submission/Kimdongha96/01_first_webapp/app.py
+++ submission/Kimdongha96/01_first_webapp/app.py
@@ -7,15 +7,15 @@ st.write(
     # 나에게 가장 도움이 될 것 같은 유튜브
   ## 노마드 코더
   > 이유 : 무언가 전문적인 강의를 해주는 느낌인 것 같다.
-  [https://i.ibb.co/cx9h9qY/1-9-2x-100.jpg]
-  (https://www.youtube.com/@nomadcoders/videos)
+  ![](https://i.ibb.co/cx9h9qY/1-9-2x-100.jpg)
+  
 
   
   # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
   ## 조코딩
   > 이유 : 구독자수와 조회수가 잘 나오고 이해하기 쉽게 설명을 해주는 느낌이다.
-  (https://www.youtube.com/@jocoding)
-  
+  ![](https://yt3.googleusercontent.com/JuRcVt9OFQgqh7UL1LjihpVLEbjdNXt3tGq-IQfqRMT8wVXgWg_tzyz0S_GVsgqkB3ucBC5fqeY=s900-c-k-c0x00ffffff-no-rj)
+
 
 
   #참고사이트
