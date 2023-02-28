28e17c266f430a614011d953e93af08c44cd5f29
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 15:21:34 +0900

    웹앱

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -4,52 +4,35 @@ import streamlit as st
 # 마크다운을 기반으로 한 꾸미기 기능 작동
 st.write(
     """
+    ---
     # 나에게 가장 도움이 될 것 같은 유튜브
 
-    ##
+    ## 노마드코더
 
-    >>이유:
+    [![이미지 설명](https://i.ytimg.com/vi/Cnbmrh99c1o/hqdefault.jpg)](https://www.youtube.com/@nomadcoders)
 
-    ## 부족하지만 많이 사랑해주세요!
+    >>이유: 영어로 IT지식을 배울수 있어서
+
+    ---
 
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
 
-    ##
+    ## 드림코딩
 
-    >>이유:
+    [![이미지 설명](https://i.ytimg.com/vi/TTLHd3IyErM/hqdefault.jpg)](https://www.youtube.com/@dream-coding)
 
-    * 1$ = 1,300원
-    * ^_^
-    #나에게 가장 도움이 될 것 같은 유튜브
-  ## 빵형의 개발도상국
-  > 이유 : 새로운 머신러닝/딥러닝 지식들이 많다
-  
-  # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
-  ## 노마드코더
-  > 이유 : 최신 IT 트렌드를 쉽고 재밌게 전달해준다
+    >>이유: 웹개발 프론트엔드의 기초부터 심화까지 배울 수 있음
+
+    ---
+
   #참고사이트
   [icons8](https://icons8.com)
   [flaticon](https://flaticon.com)
   [pixabay](https://pixabay.com)
-   
-    #### 나에게 가장 도움이 될 것 같은 유튜브
-    ### 퇴근후딴짓
-    **이유** : 제목이 마음에 듬.
-    
-    #### 다른 학생들에게 가장 도움이 될 것 같은 유튜브
-    ### 드림코딩
-    **이유** : 친절.
+  
     
     ![이미지 설명] https://icons8.com/illustrations/illustration/demure-man-programs-on-laptop-while-sitting-in-an-armchair-1
     ![남자] https://icons8.com/illustrations/illustration/demure-man-programs-on-laptop-while-sitting-in-an-armchair-1
     
     """
-)
-
-# 숙제
-# 유튜브 추천 (for me, for classmate)
-
-# https://pixabay.com/ko
-st.image(
-    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg"
 )
