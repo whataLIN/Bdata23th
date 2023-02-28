d12e91c874487f399ee694f2886e806cba19c2a3
Byungyeon Kho <59509808+mastgm0817@users.noreply.github.com>
2023-02-28 15:24:04 +0900

    웹앱

--- submission/mastgm0817/01_first_webapp/app.py
+++ submission/mastgm0817/01_first_webapp/app.py
@@ -7,32 +7,39 @@ st.write(
     ---
     # 나에게 가장 도움이 될 것 같은 유튜브
 
-    ## 노마드코더
+    > 노마드코더
 
     [![이미지 설명](https://i.ytimg.com/vi/Cnbmrh99c1o/hqdefault.jpg)](https://www.youtube.com/@nomadcoders)
 
-    >>이유: 영어로 IT지식을 배울수 있어서
+    ```
+    이유: 영어로 IT지식을 배울수 있어서
+  
+    ```
+
+    ~~~영어공부해야겠다는 생각이 듦~~~
 
     ---
 
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
 
-    ## 드림코딩
+    > 드림코딩
 
     [![이미지 설명](https://i.ytimg.com/vi/TTLHd3IyErM/hqdefault.jpg)](https://www.youtube.com/@dream-coding)
 
-    >>이유: 웹개발 프론트엔드의 기초부터 심화까지 배울 수 있음
+    ```
+    이유: 웹개발 프론트엔드의 기초부터 심화까지 배울 수 있음
+    
+    ```
+
+    ~~~웹 개발 공부 지옥에 발 담근..~~~
 
     ---
 
-  #참고사이트
-  [icons8](https://icons8.com)
-  [flaticon](https://flaticon.com)
-  [pixabay](https://pixabay.com)
+  # 참고사이트
+  + [icons8](https://icons8.com)
+  + [flaticon](https://flaticon.com)
+  + [pixabay](https://pixabay.com)
   
-    
-    ![이미지 설명] https://icons8.com/illustrations/illustration/demure-man-programs-on-laptop-while-sitting-in-an-armchair-1
-    ![남자] https://icons8.com/illustrations/illustration/demure-man-programs-on-laptop-while-sitting-in-an-armchair-1
-    
+
     """
 )
